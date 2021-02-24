27a88756909986d1e73b781c01e90cf7445c1143
HarrisMBrad <bmichaelh13@gmail.com>
2021-02-24 11:18:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Scroll from './components/scroll/scroll.component';
+import SearchBox from './components/search-box/searchbox.component';
+import CardList from './components/card-list/cardlist.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+    //order of execution...
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response =>
+        response.json())
+      .then(users => this.setState({ monsters: users }));
+    // when the state updated from what was fetched...
+    console.log('componentDidMount');
+  }
+
+
+  /**Remmember, this render method is already provided 
+   * we just come above it to make the function component */
+  render() {
+    // Destructuring the state...
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
+    // for deveopment...
+    console.log('render');
+    return (
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e =>
+            this.setState({ searchField: e.target.value })} />
+        <Scroll>
+          <CardList monsters={filteredMonsters} />
+        </Scroll>
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
